abfede3e614dc91c72772201557fb2d3cffb8030
Andrew <wing_andrew@hotmail.com>
2021-08-12 13:39:02 -0400

    Fixing models

--- models/workout.js
+++ models/workout.js
@@ -1,44 +1,36 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    
+const workOutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
     },
-    exercises: [
-        {
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter the type of workout"
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter a name for this workout"
-        },
-        duration: {
-            type: Number,
-            required: "Enter a duration for the workout"
-        },
-        weight:{
-            type: Number
-        },
-        reps:{
-            type: Number
-        },
-        sets:{
-            type: Number
-        },
-        distance:{
-            type: Number
-        },
+    exercises: [{
+      type: {
+        type: String
+      },
+      name: {
+        type: String
+      },
+      duration: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      },
+      weight: {
+        type:Number
+      },
+      reps:{
+        type: Number
+      },
+      sets: {
+        type: Number
+      }
+    }],
+});
 
-    }
-    ]
-})
+const Workout = mongoose.model('Workout', workOutSchema);
 
-const Workout = mongoose.model('Workout', workoutSchema);
+module.exports = Workout;
